aff5b9e8f70a186a2cd49abf46cb09142872e618
Julien <julien.sabathe@ynov.com>
2025-04-11 15:04:53 +0200

    [ADD] File Main Python

---
+++ python.py
@@ -0,0 +1,12 @@
+from auth import get_gmail_service
+from email_processor import process_emails
+from logger import logger
+
+if __name__ == '__main__':
+    try:
+        logger.info("Démarrage du script...")
+        service = get_gmail_service()
+        process_emails(service)
+        logger.info("Script exécuté avec succès!")
+    except Exception as e:
+        logger.critical(f"Échec critique: {str(e)}")
