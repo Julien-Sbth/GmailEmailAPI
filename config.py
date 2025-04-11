2d6764a0f7ac6e7de4d5edd6bf68d955ec01d544
Julien <julien.sabathe@ynov.com>
2025-04-11 15:10:29 +0200

    [ADD] Config Token, Email Query

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+TOKEN_FILE = os.getenv('GMAIL_TOKEN_FILE', 'token.json')
+CREDENTIALS_FILE = os.getenv('GMAIL_CREDENTIALS_FILE', 'credentials.json')
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+EMAIL_QUERY = """
+(subject:("Suite à votre candidature" OR "Votre candidature" OR "Alternance" OR "Candidature spontanée"
+           OR "Réponse candidature" OR "Votre postulation" OR "Votre demande")
+ OR body:("Suite à notre rencontre" OR "entretien" OR "poste" OR "recrutement"
+          OR "processus de recrutement" OR "votre profil" OR "nous étudions votre candidature"))
+-label:Candidatures
+"""
+
+PROCESSING_CONFIG = {
+    'days_to_search': 90,
+    'batch_size': 100,
+    'target_label': "Candidatures"
+}
